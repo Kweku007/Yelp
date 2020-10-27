2f1866a7594fe748f356ab8852231b018a444223
Kweku007 <kna38@drexel.edu>
2020-10-27 09:51:22 -0400

    Added infinite scrolling feature

--- Yelpy/Controllers/RestaurantsViewController.swift
+++ Yelpy/Controllers/RestaurantsViewController.swift
@@ -9,14 +9,57 @@
 import UIKit
 import AlamofireImage
 
-class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class InfiniteScrollActivityView: UIView {
+    var activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView()
+    static let defaultHeight:CGFloat = 60.0
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupActivityIndicator()
+    }
+    
+    override init(frame aRect: CGRect) {
+        super.init(frame: aRect)
+        setupActivityIndicator()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        activityIndicatorView.center = CGPoint(x: self.bounds.size.width/2, y: self.bounds.size.height/2)
+    }
+    
+    func setupActivityIndicator() {
+        activityIndicatorView.style = UIActivityIndicatorView.Style.medium
+        activityIndicatorView.hidesWhenStopped = true
+        self.addSubview(activityIndicatorView)
+    }
+    
+    func stopAnimating() {
+        self.activityIndicatorView.stopAnimating()
+        self.isHidden = true
+    }
+    
+    func startAnimating() {
+        self.isHidden = false
+        self.activityIndicatorView.startAnimating()
+    }
+}
+
+
+class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UIScrollViewAccessibilityDelegate{
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var isMoreDataLoading = false
+    var loadingMoreView:InfiniteScrollActivityView?
+    
     
     // ––––– TODO: Build Restaurant Class
     
     // –––––– TODO: Update restaurants Array to an array of Restaurants
     var restaurantsArray: [Restaurant] = []
+    //var filteredData: [String]!
     
     
     override func viewDidLoad() {
@@ -24,7 +67,18 @@ class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableV
 
         tableView.delegate = self
         tableView.dataSource = self
+        //searchBar.delegate = self
         getAPIData()
+        
+        // Set up Infinite Scroll loading indicator
+        let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height:InfiniteScrollActivityView.defaultHeight)
+        loadingMoreView = InfiniteScrollActivityView(frame: frame)
+        loadingMoreView!.isHidden = true
+        tableView.addSubview(loadingMoreView!)
+            
+        var insets = tableView.contentInset
+        insets.bottom += InfiniteScrollActivityView.defaultHeight
+        tableView.contentInset = insets
     }
     
     
@@ -72,9 +126,71 @@ class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableV
         
         
     }
+    
+    func loadMoreData() {
+
+        // ... Create the NSURLRequest (myRequest) ...
+        // ––––– TODO: Add your own API key!
+        let apikey = "w8FygWLPm9WXkn0fyojy6uimVDrfEMZP3hGniKh3CjxTmh8j4QzBA65sVu0bjc-Q8ncKKagptj4nRFW7VzEOGSqPBaNMMdUpocbaSq7n9793TgP-kmsJlelwLNhuX3Yx"
+        
+        // Coordinates for San Francisco
+        let lat = 37.773972
+        let long = -122.431297
+        
+        
+        let url = URL(string: "https://api.yelp.com/v3/transactions/delivery/search?latitude=\(lat)&longitude=\(long)")!
+        
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        // Insert API Key to request
+        request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+
+        let session = URLSession(configuration: URLSessionConfiguration.default,
+                                    delegate:nil,
+                                    delegateQueue:OperationQueue.main
+            )
+            
+            let task : URLSessionDataTask = session.dataTask(with: request, completionHandler: { (data, response, error) in
+
+                // Update flag
+                self.isMoreDataLoading = false
+
+                // Stop the loading indicator
+                self.loadingMoreView!.stopAnimating()
+
+                // ... Use the new data to update the data source ...
+
+                // Reload the tableView now that there is new data
+                self.tableView.reloadData()
+            })
+            task.resume()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            // Handle scroll behavior here
+        
+        if (!isMoreDataLoading) {
+                // Calculate the position of one screen length before the bottom of the results
+                let scrollViewContentHeight = tableView.contentSize.height
+                let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+                
+                // When the user has scrolled past the threshold, start requesting
+                if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+                    isMoreDataLoading = true
+
+                    // Update position of loadingMoreView, and start loading indicator
+                    let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight)
+                    loadingMoreView?.frame = frame
+                    loadingMoreView!.startAnimating()
+
+                    // Code to load more results
+                    loadMoreData()
+                }
+            }
+        
+        }
 
     
-
 }
 
 
